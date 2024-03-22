3d2bc1619de79fe70d043ded901e759e7b941590
Chad96 <chadrackndalamba@gmail.com>
2024-03-22 04:01:43 -0700

    fix tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+	ensure => installed,
+}
+
+exec { 'install_flask':
+	command => '/usr/bin/pip3 install Flask==2.1.0',
+	unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+	require => Package['python3-pip'],
 }
 
